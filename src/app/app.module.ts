0f9687d195948f0af37850e3f2057edfeb318ac2
Thalis Ribeiro <thalis199@outlook.com>
2023-03-17 16:19:08 -0300

    Add componente de listagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListagemComponent } from './listagem/listagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListagemComponent
   ],
   imports: [
     BrowserModule
