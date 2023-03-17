431a36771c5dfb4c289b6e22698dfc9c8fd5a173
Thalis Ribeiro <thalis199@outlook.com>
2023-03-17 16:55:36 -0300

    Mostrando a lista de categorias

--- src/app/listagem/listagem.component.ts
+++ src/app/listagem/listagem.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gunCategory } from 'src/models/guncategory.model';
+import { DefaultDataService } from '../services/default-data.service';
 
 @Component({
   selector: 'app-listagem',
   templateUrl: './listagem.component.html',
   styleUrls: ['./listagem.component.css']
 })
-export class ListagemComponent {
+export class ListagemComponent implements OnInit{
+  public guncategories: gunCategory[] = [];
+  ngOnInit(): void {
+      let mockData :DefaultDataService = new DefaultDataService();
+      this.guncategories = mockData.getDefaultGunCategory();
+  }
 
 }
