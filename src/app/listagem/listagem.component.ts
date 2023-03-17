f85701f37996b7c05e9118bdcaaab372c50fb631
Thalis Ribeiro <thalis199@outlook.com>
2023-03-17 17:56:22 -0300

    Testando mostrar armas

--- src/app/listagem/listagem.component.ts
+++ src/app/listagem/listagem.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { gunCategory } from 'src/models/guncategory.model';
+import { Gun } from 'src/models/gun.model';
 import { DefaultDataService } from '../services/default-data.service';
 
 @Component({
@@ -9,9 +10,11 @@ import { DefaultDataService } from '../services/default-data.service';
 })
 export class ListagemComponent implements OnInit{
   public guncategories: gunCategory[] = [];
+  public gunsList: Gun[] = [];
   ngOnInit(): void {
       let mockData :DefaultDataService = new DefaultDataService();
       this.guncategories = mockData.getDefaultGunCategory();
+      this.gunsList = mockData.getDefaultGunList();
   }
 
 }
--- src/app/services/default-data.service.ts
+++ src/app/services/default-data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Gun } from 'src/models/gun.model';
 import { gunCategory } from 'src/models/guncategory.model';
 
 @Injectable({
@@ -14,6 +15,21 @@ export class DefaultDataService {
   new gunCategory('Marksman Rifle', 5), new gunCategory('Sniper Rifle', 6), 
   new gunCategory('Handguns', 7), new gunCategory('Melee', 8), new gunCategory('Launchers', 9)];
   return gunCategories;
-  }
+}
+
+public getDefaultGunList():Gun[]{
+  let guns:Gun[] =[];
+  let gun = new Gun("m4",0, '25 headshots', '25 longshots');
+  gun.setCamos('kill','kill','kill','kill',)
+  guns.push(gun);
+  gun = new Gun("m16",0, '25 headshots', '25 longshots');
+  gun.setCamos('kill','kill','kill','kill',)
+  guns.push(gun);
+  gun = new Gun("m13",0, '25 headshots', '25 longshots');
+  gun.setCamos('kill','kill','kill','kill',)
+  guns.push(gun);
   
+  return guns;
+  // gun.normalCamos[0].isUnlocked = true
+}
 }
