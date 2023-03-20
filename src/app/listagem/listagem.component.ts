6e99856ffebc0dd3a234a4ea014f2e46a626e4f8
Thalis Ribeiro <thalis199@outlook.com>
2023-03-19 22:57:04 -0300

    contador do total de golds por categoria

--- src/app/listagem/listagem.component.ts
+++ src/app/listagem/listagem.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
 import { gunCategory } from 'src/models/guncategory.model';
 import { Gun } from 'src/models/gun.model';
 import { DefaultDataService } from '../services/default-data.service';
+import { CamoCounter } from 'src/models/camos-counter.model';
+import { achievability } from 'src/models/verify-achievability';
 
 @Component({
   selector: 'app-listagem',
@@ -11,6 +14,8 @@ import { DefaultDataService } from '../services/default-data.service';
 export class ListagemComponent implements OnInit{
   public guncategories: gunCategory[] = [];
   public gunsList: Gun[] = [];
+  camoCounter = new CamoCounter;
+  achievable = new achievability;
   ngOnInit(): void {
       let mockData :DefaultDataService = new DefaultDataService();
       this.guncategories = mockData.getDefaultGunCategory();
@@ -37,5 +42,20 @@ export class ListagemComponent implements OnInit{
       return;
     }
     this.gunsList[id]._goldCamo.isUnlocked = !this.gunsList[id]._goldCamo.isUnlocked;
+    this.totalDeGold(id);
+
+  }
+  totalDeGold(id:number) {
+    switch (this.gunsList[id]._goldCamo.isUnlocked) {
+      case true:
+        this.camoCounter.goldCounter[this.gunsList[id].categoryId] += 1;
+        break;
+
+      default:
+        if(this.camoCounter.goldCounter[this.gunsList[id].categoryId]>0)
+        this.camoCounter.goldCounter[this.gunsList[id].categoryId] -= 1;
+        break;
+    }
+    console.log(this.guncategories[this.gunsList[id].categoryId].name+" possui "+this.camoCounter.goldCounter[this.gunsList[id].categoryId]+" golds")
   }
 }
