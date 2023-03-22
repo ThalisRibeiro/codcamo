30282818f296fb062cfdb51d851c801f3ef9aa22
Thalis Ribeiro <thalis199@outlook.com>
2023-03-22 20:27:15 -0300

    array conta quantas platinum e poly desbloqueadas

--- src/models/camos-counter.model.ts
+++ src/models/camos-counter.model.ts
@@ -1,6 +1,6 @@
 export class CamoCounter{
     //id igual id de categorias
     goldCounter:number[]=  [0,0,0,0,0,0,0,0,0,0];
-    platinumCounter:number = 0;
+    platPolyCounter:number[] = [0,0];
     orionCounter:number = 0;
 }
