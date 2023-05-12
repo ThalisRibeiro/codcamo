5cf1e413ad70bd783e137f93ed01fb76c16f5ef7
ThalisRibeiro <thalis199@outlook.com>
2023-05-12 14:56:58 -0300

    Criacao do routing module com as rotas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListagemComponent } from './listagem/listagem.component';
+import { AboutComponent } from './about/about.component';
+
+const routes:Routes = [
+  {path:'', component: ListagemComponent},
+  {path:'about', component: AboutComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
